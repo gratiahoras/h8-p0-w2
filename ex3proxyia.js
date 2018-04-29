1c04aceb1cd84a3a2c0cc1a84fc16d30e2cae36c
gratiahoras <gtratiahoras@gmail.com>
2018-04-29 16:15:47 +0700

    t

---
+++ ex3proxyia.js
@@ -0,0 +1,22 @@
+var nama = 'Kirito';
+var peran = 'Ksatria';
+
+if (nama === '') {
+    console.log('Nama harus diisi!');
+}
+else{
+switch (peran) {
+    case 'Ksatria':
+        console.log('Selamat datang di Dunia Proxytia, ' + nama);
+        console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+        break;
+    case 'Tabib':
+        console.log('Selamat datang di Dunia Proxytia, ' + nama + 'Halo ' + peran + ' ' + 'name' + ', akan membantu temanmu yang terluka.');
+        break;
+    case 'Penyihir':
+        console.log('Selamat datang di Dunia Proxytia, ' + nama + 'Halo ' + peran + ' ' + 'name' + ', kamu ciptakan keajaiban yang membantu kemenanganmu');
+        break;
+    default:
+        console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
+}
+}
